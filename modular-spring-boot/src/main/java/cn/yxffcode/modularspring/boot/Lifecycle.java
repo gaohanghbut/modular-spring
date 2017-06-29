2b073b5484c78420b49bec67e121741ddfe621cc
gaohanghbut <568720503@qq.com>
2017-06-30 00:00:11 +0800

    添加destroy方法

--- modular-spring-boot/src/main/java/cn/yxffcode/modularspring/boot/Lifecycle.java
+++ modular-spring-boot/src/main/java/cn/yxffcode/modularspring/boot/Lifecycle.java
@@ -1,8 +1,8 @@
 package cn.yxffcode.modularspring.boot;
 
 import com.google.common.base.Throwables;
-import com.google.common.collect.Maps;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -11,16 +11,24 @@ import java.util.Map;
 /**
  * @author gaohang on 6/24/17.
  */
-public class Bootstrap {
+public class Lifecycle {
 
   private Map<ModuleConfig, ApplicationContext> applicationContexts = Collections.emptyMap();
 
   public void boot() {
     final ModuleLoader moduleLoader = new DefaultModuleLoader();
     try {
-      this.applicationContexts = moduleLoader.load(Bootstrap.class.getClassLoader());
+      this.applicationContexts = moduleLoader.load(Lifecycle.class.getClassLoader());
     } catch (IOException e) {
       throw Throwables.propagate(e);
+    }
+  }
+
+  public void destroy() {
+    for (ApplicationContext applicationContext : applicationContexts.values()) {
+      if (applicationContext instanceof ConfigurableApplicationContext) {
+        ((ConfigurableApplicationContext) applicationContext).close();
+      }
     }
   }
 }
