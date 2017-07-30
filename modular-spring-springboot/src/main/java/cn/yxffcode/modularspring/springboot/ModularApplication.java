8a6a3d9a5e7b1ba0255bc7b597f48aaef0ce469c
gaohanghbut <568720503@qq.com>
2017-07-30 14:24:12 +0800

    for spring-boot

---
+++ modular-spring-springboot/src/main/java/cn/yxffcode/modularspring/springboot/ModularApplication.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.yxffcode.modularspring.springboot;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates a {@link Configuration configuration} class that declares one or more
+ * {@link Bean @Bean} methods and also triggers {@link EnableAutoConfiguration
+ * auto-configuration} and {@link ComponentScan component scanning}. This is a convenience
+ * annotation that is equivalent to declaring {@code @Configuration},
+ * {@code @EnableAutoConfiguration} and {@code @ComponentScan}.
+ *
+ * @author Phillip Webb
+ * @author Stephane Nicoll
+ * @since 1.2.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan(excludeFilters = {
+    @Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+    @Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class)},
+    basePackages = {"org.springframework.boot.autoconfig"}
+)
+public @interface ModularApplication {
+
+  /**
+   * Exclude specific auto-configuration classes such that they will never be applied.
+   *
+   * @return the classes to exclude
+   */
+  @AliasFor(annotation = EnableAutoConfiguration.class, attribute = "exclude")
+  Class<?>[] exclude() default {};
+
+  /**
+   * Exclude specific auto-configuration class names such that they will never be
+   * applied.
+   *
+   * @return the class names to exclude
+   * @since 1.3.0
+   */
+  @AliasFor(annotation = EnableAutoConfiguration.class, attribute = "excludeName")
+  String[] excludeName() default {};
+}
