6355a792f54a3d4311f495b33fbbfa33dbc7130a
gaohanghbut <568720503@qq.com>
2018-09-23 18:05:37 +0800

    opt imports

--- modular-spring-core/src/main/java/cn/yxffcode/modularspring/core/annotation/ModularService.java
+++ modular-spring-core/src/main/java/cn/yxffcode/modularspring/core/annotation/ModularService.java
@@ -2,11 +2,7 @@ package cn.yxffcode.modularspring.core.annotation;
 
 import org.springframework.stereotype.Component;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * 发布服务
